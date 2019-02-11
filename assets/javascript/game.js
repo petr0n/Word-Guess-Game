4fba00e289698ead00b57407aeddc93ef70ce4a7
petr0n <petron@gmail.com>
2019-02-10 20:04:34 -0600

    add gitignore and loads of images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -52,108 +52,124 @@ var wordList = [
 'banana'
 ];
 
-(function(){
-    var game = {
-        init: function() {
-            this.matchingArr = [];
-            this.nonMatchingArr = [];
-            guessedArr = [];
-            correctGuessesArr = [];
-            indicesArr = [];
-            guessCt = 1;
-            wins = 0;
-            loses = 0;
-            this.nonMatchEl = document.getElementById('non-matching');
-            wordToGuessEl = document.getElementById('word-to-guess');
-            spanEl = document.createElement('span');
-            totalGuessesEl = document.getElementById('total-guesses');
-            winsEl = document.getElementById('wins');
-            losesEl = document.getElementById('loses');
-            this.setup();
-            this.listener(); // start listening for keyup events
-        },
-        play: function(letter) {
-            guessedArr.push(this.letter);
-            totalGuessesEl.innerHTML = guessCt;
-            // more than one instance of the letter?
-            console.log(this.randomWord + '--' + letter)
-            indicesArr = this.indexesOf(this.randomWord, letter);
-            guessCt++;
-            if (indicesArr.length) { // has one or more letters in word
-                indicesArr.map(function(x){
-                    thisSpan = spanEl.cloneNode(true);
-                    thisSpan.innerHTML = letter;
-                    wordToGuessEl.replaceChild(thisSpan, wordToGuessEl.childNodes[x]);
-                    correctGuessesArr[x] = letter;
-                });
-                correctGuess = correctGuessesArr.join('');
-                if (correctGuess == this.randomWord) {
-                    var w = ++wins;
-                    winsEl.innerHTML = w;
-                    var newGameWin = confirm('GAME OVER! \r\nYOU WIN! Congrats, mi amigo. \r\n\r\n Play again?');
-                    if (newGameWin) this.reset();
-                }
-            } else { // letter not in word 
-                if (this.nonMatchingArr.indexOf(letter) > -1){
-                } else {
-                    p = spanEl.cloneNode(true);
-                    p.innerHTML = letter;                
-                    this.nonMatchEl.appendChild(p);
-                    this.nonMatchingArr.push(letter);
-                }
-            }
 
-            // console.log('this.nonMatchingArr.length: ' +this.nonMatchingArr.length);
-            if (this.nonMatchingArr.length > 4) {
-                var newGameLose = confirm('GAME OVER! \r\nOh no, you lose! \r\n\r\n Play again?');
-                var l = ++loses;
-                losesEl.innerHTML = l;
-                if (newGameLose) this.reset();
-            }
-        },
-        listener: function(){
-            var _this = this;
-            document.addEventListener('keyup', function(event){ 
-                var char = String.fromCharCode(event.keyCode);
-                var isAlpha = char.match(/[a-zA-Z]/g);
-                this.letter = char.toLowerCase();
-                if (isAlpha) {
-                    var play = new _this.play(this.letter);
-                    //_this.play() ;
-                }
-            });
-        },
-        setup: function() {
-            this.randomWord = wordList[Math.floor(Math.random() * wordList.length)];
-            
-            console.log('new word: ' + this.randomWord);
-            
-            for (var x = 0; x < this.randomWord.length; x++) {
-                p = spanEl.cloneNode(true);
-                p.innerHTML = '&nbsp;';
-                wordToGuessEl.appendChild(p);
-            }
-        },
-        reset: function(){
-            while (wordToGuessEl.firstChild) { // empty node
-                wordToGuessEl.removeChild(wordToGuessEl.firstChild);
-            }
-            while (this.nonMatchEl.firstChild) { // empty node
-                this.nonMatchEl.removeChild(this.nonMatchEl.firstChild);
-            }
-            totalGuessesEl.innerHTML = '';
-            this.init();
-        },
-        indexesOf: function(string, char) { // helper function
-            var indices = [];
-            console.log(string + '  === ' + char);
-            for(var i=0; i < string.length;i++) {
-                if (string[i] === char) indices.push(i);
-            }
-            return indices;
+var game = {
+    settings: {
+        guessedArr: [],
+        incorrectGuessesArr: [],
+        correctGuessesArr: [],
+        indicesArr: [],
+        guessCt: 0,
+        wins: 0,
+        losses: 0,
+        nonMatchEl: document.getElementById('non-matching'),
+        wordToGuessEl: document.getElementById('word-to-guess'),
+        spanEl: document.createElement('span'),
+        totalGuessesEl: document.getElementById('total-guesses'),
+        winsEl: document.getElementById('wins'),
+        lossesEl: document.getElementById('losses'),
+        man: ['noose', 'head', 'body', 'left-arm', 'right-arm', 'left-leg', 'right-leg']
+    },
+    init: function(){
+        s = this.settings;
+        var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+        // console.log('new word: ' + randomWord);
+        for (var x = 0; x < randomWord.length; x++) {
+            var p = s.spanEl.cloneNode(true);
+            p.innerHTML = '&nbsp;';
+            s.wordToGuessEl.appendChild(p);
         }
-    };
+        game.listener(randomWord);  
+    },
+    play: function(randomWord, letter) {
+        if (s.guessedArr.indexOf(letter) === -1) {
+            s.guessedArr.push(letter);
+            s.totalGuessesEl.innerHTML = s.guessedArr.length;
+        }
+        // console.log(s.guessedArr.indexOf(letter) !== -1);
+        // console.log('s.guessedArr: ');
+        // console.log(s.guessedArr);
+
+        console.log(randomWord + '--' + letter);
+        // get all indexes of the letter
+        s.indicesArr = game.indexesOf(randomWord, letter);
+        if (s.indicesArr.length) { // has one or more letters in word
+            s.indicesArr.map(function(x){
+                var thisSpan = s.spanEl.cloneNode(true);
+                thisSpan.innerHTML = letter;
+                s.wordToGuessEl.replaceChild(thisSpan, s.wordToGuessEl.childNodes[x]);
+                s.correctGuessesArr[x] = letter;
+            });
+            var correctGuess = s.correctGuessesArr.join('');
+            console.log('correctGuess: ' + correctGuess);
+            if (correctGuess == randomWord) {
+                s.wins++;
+                s.winsEl.innerHTML = s.wins;
+                var newGameWin = confirm('GAME OVER! \r\nYOU WIN! Congrats, mi amigo. \r\n\r\n Play again?');
+                if (newGameWin) {
+                    game.reset();
+                    game.init();
+                }
+            }
+        } else { // letter not in word 
+            if (s.incorrectGuessesArr.indexOf(letter) > -1){
+            } else {
+                var p = s.spanEl.cloneNode(true);
+                p.innerHTML = letter;                
+                s.nonMatchEl.appendChild(p);
+                s.incorrectGuessesArr.push(letter);
+                // make man appear 
+                if (s.incorrectGuessesArr.length !== 7){
+                    var part = document.getElementsByClassName(s.man[s.incorrectGuessesArr.length-1]);
+                    part[0].style.display = "block";
+                }
+            }
+            if (s.incorrectGuessesArr.length > 6) {
+                var newGameLose = confirm('GAME OVER! \r\nOh no, you lose! \r\n\r\n Play again?');
+                s.losses++;
+                s.lossesEl.innerHTML = s.losses;
+                if (newGameLose) {
+                    game.reset();
+                    game.init();
+                }
+            }
+        }
+    },
+    listener: function(randomWord){
+        _myListener = function(event){ 
+            var char = String.fromCharCode(event.keyCode);
+            var isAlpha = char.match(/[a-zA-Z]/g);
+            this.letter = char.toLowerCase();
+            if (isAlpha) {
+                game.play(randomWord, this.letter);
+            }
+        };
+        document.addEventListener('keyup', _myListener, false);
+    },
+    reset: function(){
+        while (s.wordToGuessEl.firstChild) { // empty node
+            s.wordToGuessEl.removeChild(s.wordToGuessEl.firstChild);
+        }
+        while (s.nonMatchEl.firstChild) { // empty node
+            s.nonMatchEl.removeChild(s.nonMatchEl.firstChild);
+        }
+        s.incorrectGuessesArr = [];
+        s.totalGuessesEl.innerHTML = '';
+        document.removeEventListener('keyup', _myListener, false);
+    },
+    indexesOf: function(string, char) { // helper function
+        var indices = [];
+        // console.log(string + '  === ' + char);
+        for(var i=0; i < string.length;i++) {
+            if (string[i] === char) indices.push(i);
+        }
+        return indices;
+    }
+};
+(function(){
     game.init();
-    
 })();
 
+function popModal(title,body){
+
+}
